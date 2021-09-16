b647bc4d9347bc2da7b1b575c78f4199ea9b4ee9
Sergey Terentyev <sergejs.terentjevs@gmail.com>
2021-09-16 12:10:48 +0100

    Displaying items on the form

--- app/controllers/GoogleSupportArticlesController.scala
+++ app/controllers/GoogleSupportArticlesController.scala
@@ -1,15 +1,14 @@
 package controllers
 
 import akka.actor.ActorSystem
-import articles.Product._
-import articles.{Article, Product}
-import play.api.libs.json.Json
-
-import java.net.URI
-//import articles.Scrape._
+import articles.Scrape
+import articles.Scrape._
 import com.google.inject.Singleton
 import config.ScrapingConfig
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import scalaz.Functor
+import scalaz.Scalaz.futureInstance
 
 import javax.inject.Inject
 import scala.concurrent.Future
@@ -26,17 +25,11 @@ class GoogleSupportArticlesController @Inject()(val controllerComponents: Contro
   }
 
   def scrape(): Action[AnyContent] = Action.async { implicit request =>
-    //    val builder = Scrape(config.googleSupportUri) limitConcurrency config.maxConcurrent
-    //    val items: Future[Seq[Product]] = builder
+    def scrapeArticles =
+      Scrape(config.googleSupportUri) limitConcurrency config.maxConcurrent
 
-    Future.successful(
-      Ok(
-        Json.toJson(
-          Seq(
-            Product("A", Seq(Article("Ok", URI.create("http://ok.com"))))
-          )
-        )
-      )
-    )
+    Functor[Future].map(scrapeArticles) { items =>
+      Ok(Json.toJson(items))
+    }
   }
 }
